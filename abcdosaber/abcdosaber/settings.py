bbba641046f732deae87eef14f0c603a25390297
kapuerista <bittencourtvilelaigor@gmail.com>
2026-05-22 21:45:31 -0300

    Aula 05

--- abcdosaber/abcdosaber/settings.py
+++ abcdosaber/abcdosaber/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'titulo.apps.TituloConfig',
+    # 'titulo.apps.TituloConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'abcdosaber.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
